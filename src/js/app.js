908135da989bc164218e560ae82be3924b362804
Seven00321 <richrdpere123@gmail.com>
2023-12-06 18:15:50 -0500

    3er commit del PROYECTO desde la OFICINA

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     eventListeners();
+    darkMode();
+
 });
 
+function darkMode(){
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    //console.log(prefiereDarkMode.matches);
+
+    // Funcion para cambiar a Dark Mode
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    }
+    else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    // Funcion para cambiar a Dark Mode con el SO
+    prefiereDarkMode.addEventListener('change', function(){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        }
+        else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+    });
+
+}
+
 function eventListeners(){
-    const mobileMenu = document.querySelector('.mobileMenu');
+    const mobileMenu = document.querySelector('.mobile-menu');
 
     mobileMenu.addEventListener('click', navegacionResponsive);
 }
